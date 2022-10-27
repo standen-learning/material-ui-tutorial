cda48e447941f8511879c99641afc324fb4c0c7e
standen1 <standen.sean@gmail.com>
2022-10-27 11:05:05 -0700

    Initial Commit.

---
+++ src/pages/Create.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export default function Create() {
+  return (
+    <div>
+      <Typography>
+        Create a New Note
+      </Typography>
+    </div>
+  )
+}
