c4952f531da55566a6a325beba85b30dd2b79a24
standen1 <standen.sean@gmail.com>
2022-10-27 20:10:59 +0000

    Added a form.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import { Typography, Button, Container} from '@mui/material'
+import React, { useState } from 'react'
+import { Typography, Button, Container, TextField } from '@mui/material'
 // import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
 // import SendIcon from '@mui/icons-material/Send';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
+const styles = {
+  field: {
+    marginTop: 5,
+    marginBottom: 5,
+    display: 'block'
+  }
+}
 
 export default function Create() {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === '') {
+      setTitleError(true);
+    }
+
+    if(details === '') {
+      setDetailsError(true);
+    }
+
+    if(title && details) {
+      console.log(title, details);
+    }
+  }
+
   return (
     <Container>
         <Typography
@@ -17,15 +47,43 @@ export default function Create() {
           Create a New Note
         </Typography>
 
-        <Button 
-          type="submit"
-          color="primary"
-          variant="contained"
-          onClick={() => console.log("You clicked me.")}
-          endIcon={ <KeyboardArrowRightIcon /> }
-        >
-          Submit
-        </Button>
+        <form
+          noValidate 
+          autoComplete="off"
+          onSubmit={handleSubmit}
+      >
+          <TextField 
+            onChange={(e) => setTitle(e.target.value)}
+            sx={styles.field}
+            label="Note Title"
+            variant='outlined'
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+          />
+          <TextField 
+            onChange={(e) => setDetails(e.target.value)}
+            sx={styles.field}
+            label="Details"
+            variant='outlined'
+            color="secondary"
+            multiline
+            rows={4}
+            fullWidth
+            required
+            error={detailsError}
+          />
+
+          <Button 
+            type="submit"
+            color="primary"
+            variant="contained"
+            endIcon={ <KeyboardArrowRightIcon /> }
+          >
+            Submit
+          </Button>
+        </form>
 
         {/* <ButtonGroup color="secondary" variant="contained">
           <Button>One</Button>
