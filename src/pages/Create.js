f01fbde5158548f343d540f1f226069e532254c3
standen1 <standen.sean@gmail.com>
2022-10-27 18:51:42 +0000

    Icons.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Typography, Button, Container} from '@mui/material'
+// import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+// import SendIcon from '@mui/icons-material/Send';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -18,6 +21,7 @@ export default function Create() {
           color="secondary"
           variant="contained"
           onClick={() => console.log("You clicked me.")}
+          endIcon={ <KeyboardArrowRightIcon /> }
         >
           Submit
         </Button>
@@ -27,6 +31,13 @@ export default function Create() {
           <Button>Two</Button>
           <Button>Three</Button>
         </ButtonGroup> */}
+
+        {/* <AcUnitOutlinedIcon />
+        <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+        <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+        <AcUnitOutlinedIcon color="action" fontSize="small" />
+        <AcUnitOutlinedIcon color="error" fontSize="small" />
+        <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
     </Container>
   )
 }
